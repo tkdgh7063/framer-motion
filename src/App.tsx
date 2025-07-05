9ab372d3f8279774e4921908a26d57b5850aeaac
tkdgh7063 <hoo412298@gmail.com>
2025-07-05 18:13:38 +0900

    Animation practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { motion } from "motion/react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -7,18 +8,24 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   height: 200px;
   width: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  text-align: center;
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box></Box>
+      <Box
+        initial={{ scale: "0" }}
+        transition={{ type: "spring", delay: 0.5 }}
+        animate={{ scale: 1, rotateZ: 360 }}>
+        Box
+      </Box>
     </Wrapper>
   );
 }
