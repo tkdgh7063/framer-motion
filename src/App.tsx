b90e0ae91703b6bf8137c8d18e6b3167a84b731f
tkdgh7063 <hoo412298@gmail.com>
2025-07-18 05:58:23 +0900

    Slider practice
    Slider practice using AnimatePresence with state

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
@@ -16,32 +17,57 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   position: absolute;
-  top: 100px;
+  top: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
-  initial: { opacity: 0, scale: 0 },
-  visible: { opacity: 1, scale: 1, rotateZ: 360 },
-  exit: { opacity: 0, scale: 0, y: 80 },
+  entry: (isBack: boolean) => ({
+    x: isBack ? -500 : 500,
+    opacity: 0,
+    scale: 0,
+  }),
+  center: { x: 0, opacity: 1, scale: 1, transition: { duration: 0.5 } },
+  exit: (isBack: boolean) => ({
+    x: isBack ? 500 : -500,
+    opacity: 0,
+    rotateX: 90,
+    transition: { duration: 0.5 },
+  }),
 };
 
 function App() {
-  const [showing, setShowing] = useState(true);
-  const toggle = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(0);
+  const [back, setBack] = useState(false);
+  const prev = () => {
+    setVisible((prev) => (prev === 0 ? 0 : prev - 1));
+    setBack(true);
+  };
+  const next = () => {
+    setVisible((prev) => (prev === 9 ? 9 : prev + 1));
+    setBack(false);
+  };
   return (
     <Wrapper>
-      <button onClick={toggle}>{showing ? "Hide" : "Show"}</button>
-      <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="exit"
-          />
-        ) : null}
+      <AnimatePresence mode="wait">
+        <Box
+          custom={back}
+          key={visible}
+          variants={boxVariants}
+          initial="entry"
+          animate="center"
+          exit="exit">
+          {visible}
+        </Box>
       </AnimatePresence>
+      <>
+        {visible > 0 && <button onClick={prev}>Previous</button>}
+        {visible < 9 && <button onClick={next}>Next</button>}
+      </>
     </Wrapper>
   );
 }
