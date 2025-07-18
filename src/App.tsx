01fec42f3a20b2d10d087fb6e1427b17f44356e3
tkdgh7063 <hoo412298@gmail.com>
2025-07-18 22:01:45 +0900

    layout animation practice using layoutId
    Connected two different components using the same layoutId to enable smooth shared layout transitions.
    Applied motion animations for mounting and unmounting elements, improving the user experience with seamless visual continuity.

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,48 @@
 import styled from "styled-components";
-import { AnimatePresence, motion } from "motion/react";
+import { motion } from "motion/react";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
 `;
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 250px;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  entry: (isBack: boolean) => ({
-    x: isBack ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: { x: 0, opacity: 1, scale: 1, transition: { duration: 0.5 } },
-  exit: (isBack: boolean) => ({
-    x: isBack ? 500 : -500,
-    opacity: 0,
-    rotateX: 90,
-    transition: { duration: 0.5 },
-  }),
-};
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  width: 100px;
+  height: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
-  const [visible, setVisible] = useState(0);
-  const [back, setBack] = useState(false);
-  const prev = () => {
-    setVisible((prev) => (prev === 0 ? 0 : prev - 1));
-    setBack(true);
-  };
-  const next = () => {
-    setVisible((prev) => (prev === 9 ? 9 : prev + 1));
-    setBack(false);
-  };
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      <AnimatePresence mode="wait">
-        <Box
-          custom={back}
-          key={visible}
-          variants={boxVariants}
-          initial="entry"
-          animate="center"
-          exit="exit">
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <>
-        {visible > 0 && <button onClick={prev}>Previous</button>}
-        {visible < 9 && <button onClick={next}>Next</button>}
-      </>
+    <Wrapper onClick={toggleClicked}>
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: "50%" }} />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+        ) : null}
+      </Box>
     </Wrapper>
   );
 }
